5701bcb34121e2117723665947c2e4ad0081e7b2
ADARSH MENON <88380337+Adarsh-Menon@users.noreply.github.com>
2022-05-11 16:32:20 +0000

    few

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,9 @@
 # Strings
+def str():
+    text = "X-DSPAM-Confidence:    0.8475"
+    string=text.find("0.8475")
+    answer=text[string::1]
+    finalanswer=float(answer)
+    print(finalanswer)
 
-text = "X-DSPAM-Confidence:    0.8475"
+str()
--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,17 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+def file():
+  f=open("dataset/mbox-short.txt")
+  count = 0
+  sum=0
+  for line in f:
+      if line.startswith('X-DSPAM-Confidence'):
+          count = count + 1
+          x=float(line[20::1])
+          sum=sum+x
+  print('There were', count, 'subject lines in', "dataset/mbox-short.tx")
+  avg=sum/(count)
+  print("Average spam confidence: ",avg)
+  
+
+file()
