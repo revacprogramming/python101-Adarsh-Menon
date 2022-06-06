9f0fde0f57a4a52456336c3c2b6cbc6a3356a33b
ADARSH MENON <88380337+Adarsh-Menon@users.noreply.github.com>
2022-06-06 06:42:00 +0000

    few

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,19 +1,14 @@
 
 def get_cs():
-  s=int(input("how many string do you need?"))
-  l=[]
-  for i in range(0,s):
-    s=input("enter string")
-    l.append(s)
-  return l
+  s=str(input("enter the str"))
+  return s
 
 
 def cs_to_lot(l):
-  n=[]
-  for i in l:
-    n.append((i,i[0]))
-
-  return n
+  s.split(";")
+  for i in s:
+    i.split("=")
+    print(i[0],i[1])
     
     
 
