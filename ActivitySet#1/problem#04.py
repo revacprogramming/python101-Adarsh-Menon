8ad66ffa48aed5e95ab55c104ea003f13cc98a8c
ADARSH MENON <88380337+Adarsh-Menon@users.noreply.github.com>
2022-06-30 10:15:46 +0000

    few

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -9,6 +9,7 @@ def pay(hrs,rph):
   if h<=40:
 	  result = h*r
   else:
+    #
     o = h-40
     result = o*(r*1.50) + 40*r
   return result
--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,6 +1,23 @@
 # Tuples
+def tupl():
+  filename = "dataset/mbox-short.txt"
+  han = open('dataset/romeo.txt')
+
+  for line in han:
+    line = line.rstrip()
+    wds = line.split()
+    for w in wds:
+        di[w] = di.get(w,0) + 1
+
+  largest = -1
+  theword = None
+  for k,v in di.items():
+    if v is largest:
+      largest = v
+      theword = k
+
+  print(theword, largest)
 
 
-def 
-filename = open("dataset/mbox-short.txt")
+tupl
 
