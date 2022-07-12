e60c20669f525dc1bfe75a7c0af2d67360fc0320
ADARSH MENON <88380337+Adarsh-Menon@users.noreply.github.com>
2022-07-12 16:23:39 +0000

    few

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -5,7 +5,8 @@ class Menu:
 
 
 m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
+m = m + ("idly", 10) + ("vada", 20)  
+# Hint: operator overloading special methods? (__add__, __sub__, etc.)
 
 print(m)  # should print the menu properly
 def __init__(self,add_menu,price_menu):
